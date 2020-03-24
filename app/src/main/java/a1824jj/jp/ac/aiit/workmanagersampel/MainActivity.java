511bf3f4a6db4e38b90ead34e3f14e1b4adba3d8
a1824jj <a1824jj@aiit.ac.jp>
2020-03-24 10:58:08 +0900

    get worker status

--- app/src/main/java/a1824jj/jp/ac/aiit/workmanagersampel/MainActivity.java
+++ app/src/main/java/a1824jj/jp/ac/aiit/workmanagersampel/MainActivity.java
@@ -1,13 +1,16 @@
 package a1824jj.jp.ac.aiit.workmanagersampel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 import androidx.work.Worker;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +22,25 @@ public class MainActivity extends AppCompatActivity {
        final OneTimeWorkRequest oneTimeWorkRequest
                = new OneTimeWorkRequest.Builder(DemoWorker.class).build();
 
+       final TextView textView = findViewById(R.id.textView);
 
-        (findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+
+
+       (findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 WorkManager.getInstance(MainActivity.this).enqueue(oneTimeWorkRequest);
             }
-        });
+       });
+
+       WorkManager.getInstance(this).getWorkInfoByIdLiveData(oneTimeWorkRequest.getId())
+               .observe(this, new Observer<WorkInfo>() {
+                   @Override
+                   public void onChanged(WorkInfo workInfo) {
+                       if(workInfo != null){
+                           textView.setText(workInfo.getState().name());
+                       }
+                   }
+               });
     }
 }
